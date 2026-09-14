04644d5558ea8601a5111b9cbceee9b194d74b68
ArgainTobias <argaintobias@DESKTOP-0DHNFFG.localdomain>
2026-09-14 13:33:10 +0000

    Get-Personajes-Cache y Get-Personajes-Api

--- powershell/ejercicio5.ps1
+++ powershell/ejercicio5.ps1
@@ -1,6 +1,64 @@
+
 Param(
-    $id,
-    $nombre
+
+    [Parameter(Mandatory=$false)]
+    [int[]]$idsPersonajes, 
+
+    [Parameter(Mandatory=$false)]
+    [int[]]$idsPeliculas
 )
 
-Write-Host "Name: $($(Invoke-RestMethod https://www.swapi.tech/api/people/$id).result.properties.name)"
+$RUTA_CACHE_PERSONAJES="$PSScriptRoot/cache.jsonl"
+
+function Get-Personajes-Cache(){ 
+
+    if(Test-Path $RUTA_CACHE_PERSONAJES){
+        $personajesCache = Get-Content $RUTA_CACHE_PERSONAJES | ConvertFrom-Json 
+    }
+    else{
+        New-Item -Path $RUTA_CACHE_PERSONAJES -ItemType File
+        $personajesCache = @()
+    }
+
+
+    #HASTA ACA (DEVOLVIENDO EL ARRAY) TIENE QUE SER LA GET-PERSONAJES-CACHE
+
+    $encontrados = @()
+    $noEncontrados = @()
+
+    foreach($id in $idsPersonajes){
+        $objeto = ($personajesCache | Where-Object Id -eq $id)
+
+        if($objeto -ne $NULL){
+            $encontrados += $objeto
+        }else{
+            $noEncontrados += $id
+        }
+    }
+    
+    $encontrados
+    $noEncontrados
+
+    return 
+}
+
+function Get-Personajes-Api(){
+    Param(
+        [Parameter(Mandatory=$true)]
+        [int[]] $ids
+    )
+
+    $personajesApi = @()
+
+    <#AGREGAR:
+        - TRY-CATCH POR SI FALLA LA PETICION
+        - VARIABLE CON LA URL-BASE
+    #> 
+    foreach($id in $ids){
+        $personajeObtenido = (Invoke-RestMethod https://www.swapi.tech/api/people/$id).result
+        $personajesApi += $personajeObtenido
+    }
+
+}
+
+Get-Personajes-Cache
