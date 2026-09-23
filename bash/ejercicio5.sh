5dc2ce892bcb5278e3857b73046e2e4c3566a2f5
ArgainTobias <argaintobias@DESKTOP-0DHNFFG.localdomain>
2026-09-23 23:26:45 +0000

    Terminado (falta agregar la ayuda, con ejemplos y etc)

--- bash/ejercicio5.sh
+++ bash/ejercicio5.sh
@@ -49,6 +49,8 @@ RUTA_CACHE_PELICULAS="$(pwd)/cache_peliculas.jsonl"
 URL_BASE_PERSONAJES="https://www.swapi.tech/api/people/"
 URL_BASE_PELICULAS="https://www.swapi.tech/api/films/"
 
+#------------------- FUNCIONES COMPARTIDAS --------------------
+
 function validar_parametros(){
     for n in "${peoples[@]}" "${films[@]}"
     do
@@ -71,7 +73,16 @@ function get_cache()
         touch $ruta_cache
     fi
 
-    jq -c --argjson ids_buscados "$ids_json" 'select(.Id as $id | $ids_buscados | any(. == $id))' "$ruta_cache"
+    local cache=($(cat $ruta_cache | jq -c))
+    local count_cache=$(echo ${cache[@]} | jq -s 'length')
+    
+    if [ $count_cache -ne 0 ]
+    then
+        echo ${cache[@]} | jq -c --argjson ids_buscados "$ids_json" 'select(.Id as $id | $ids_buscados | any(. == $id))'
+    else
+        echo ${cache[@]}
+    fi
+
 }
 
 function get_ids_no_encontrados(){
@@ -81,13 +92,13 @@ function get_ids_no_encontrados(){
     shift 2
     local ids=("$@")
 
-    local personajes_cache=$(get_cache $ruta_cache ${ids[@]})
+    local cache=$(get_cache $ruta_cache ${ids[@]})
     local no_encontrados=()
-    local personajes_api=()
+    local api=()
 
     for id in ${ids[@]}
     do
-        if  ! echo "$personajes_cache" | grep -q "\"Id\":\"$id\"" 
+        if  ! echo "$cache" | grep -q "\"Id\":\"$id\"" 
         then
             no_encontrados+=($id)
         fi
@@ -106,20 +117,32 @@ function get_api(){
 
     for id in ${ids[@]}
     do
-        local respuesta=$(wget -qO- "${url}${id}" | jq -c '{
-            Mass: .result.properties.mass,
-            Name: .result.properties.name,
-            Birth_year: .result.properties.birth_year,
-            Id: .result.uid,
-            Height: .result.properties.height,
-            Gender: .result.properties.gender}'
-        )
+        local respuesta=$(wget -qO- "${url}${id}" | jq -c '.result')
         response+=($respuesta)
-
     done
 
-    echo ${response[@]} | jq -sc 'sort_by(.Id | tonumber)[]'
-    
+    echo ${response[@]} | jq -sc 'sort_by(.uid | tonumber)[]'   
+}
+
+function add_to_cache(){
+
+    local ruta_cache=$1
+
+    jq -c >> $ruta_cache
+}
+
+#-------------------- FUNCIONES PERSONAJES ----------------------
+function get_personajes_formateados(){
+
+    jq -c '{
+        Mass: .properties.mass,
+        Name: .properties.name,
+        Birth_year: .properties.birth_year,
+        Id: .uid,
+        Height: .properties.height,
+        Gender: .properties.gender
+    }'
+
 }
 
 function get_personajes_by_id(){
@@ -136,21 +159,14 @@ function get_personajes_by_id(){
         return
     fi
 
-    local no_encontrados=($(get_ids_no_encontrados "$RUTA_CACHE_PERSONAJES" "$URL_BASE_PERSONAJES" ${peoples[@]}))
+    local no_encontrados=($(get_ids_no_encontrados "$RUTA_CACHE_PERSONAJES" "$URL_BASE_PERSONAJES" ${ids[@]}))
     local personajes_api=($(get_api $URL_BASE_PERSONAJES ${no_encontrados[@]}))
+    local api_formateados=($(echo ${personajes_api[@]} | get_personajes_formateados))
+    local todos=(${personajes_cache[@]} ${api_formateados[@]})
 
-    local todos=(${personajes_cache[@]} ${personajes_api[@]})
-
-    echo ${personajes_api[@]} | add_to_cache $RUTA_CACHE_PERSONAJES
+    echo ${api_formateados[@]} | add_to_cache $RUTA_CACHE_PERSONAJES
 
     echo ${todos[@]}
-}
-
-function add_to_cache(){
-
-    local ruta_cache=$1
-
-    jq -c >> $ruta_cache
 }
 
 function echo_personajes(){
@@ -160,14 +176,66 @@ function echo_personajes(){
     echo "LISTADO PERSONAJES"
     echo ${personajes[@]} | jq -s 'sort_by(.Id | tonumber)[]'
 }
+#------------------- FUNCIONES PELICULAS --------------------
+function get_peliculas_formateadas(){
+
+    jq -c '{
+        Id: .uid,
+        Title: .properties.title,
+        Episode_id: .properties.episode_id,
+        Release_date: .properties.release_date,
+        Opening_crawl: .properties.opening_crawl
+    }'
+}
+
+function get_peliculas_by_id(){
+    
+    local ids=("$@")
+
+    local peliculas_cache=($(get_cache $RUTA_CACHE_PELICULAS ${ids[@]}))
+    local count_cache=$(echo ${peliculas_cache[@]} | jq -s 'length')
+    local count_ids=${#ids[@]}
+
+    if [ $count_cache -eq $count_ids ]
+    then
+        echo ${peliculas_cache[@]} | jq
+        return
+    fi
+
+    local no_encontrados=($(get_ids_no_encontrados "$RUTA_CACHE_PELICULAS" "$URL_BASE_PELICULAS" ${ids[@]}))
+    local peliculas_api=($(get_api $URL_BASE_PELICULAS ${no_encontrados[@]}))
+    local api_formateadas=($(echo ${peliculas_api[@]} | get_peliculas_formateadas))
+    local todos=(${peliculas_cache[@]} ${api_formateadas[@]})
+
+    echo ${api_formateadas[@]} | add_to_cache $RUTA_CACHE_PELICULAS
+
+    echo ${todos[@]}
+}
+
+function echo_peliculas(){
+
+    local peliculas=("$@")
+
+    echo "LISTADO PELICULAS"
+    echo ${peliculas[@]} | jq -s 'sort_by(.Id | tonumber)[]'
+}
+
 
 function main(){
 
     validar_parametros 
     if [ -n "$peoples" ]
     then
+        #get_personajes_by_id ${peoples[@]}
         personajes=($(get_personajes_by_id ${peoples[@]}))
         echo_personajes ${personajes[@]}
+    fi
+
+    if [ -n "$films" ]
+    then
+        #get_peliculas_by_id ${films[@]}
+        peliculas=($(get_peliculas_by_id ${films[@]}))
+        echo_peliculas ${peliculas[@]}
     fi
 }
 
